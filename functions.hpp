2d862e3ee4d9927aa97acc11e21e312c28762a8f
Michael J <30031129@cc.peralta.edu>
2024-11-17 22:20:06 -0800

    added all new functions to header file

--- functions.hpp
+++ functions.hpp
@@ -3,19 +3,21 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 #include "./Cards.hpp"
 #include "./User.hpp"
 
 namespace project2 {
+	person::User* makeUser(); //either creates a new user or selects a user mentioned from a data.txt file
+	void newDeck(std::vector<card::Card>& deck); //creates a full deck of cards
+	card::Card drawCard(card::Deck& deck); //draws a card from a deck
+	short playerTurn(person::User player, card::Deck& deck); //plays a round of blackjack for the user
+	std::string cardName(card::Card& card); //prints out unique card names for certain cards, like ace and face cards
+	short houseTurn(short playerScore, card::Deck& currentDeck); //simulates the turn of the house
+	void menu(person::User* user, std::shared_ptr<card::Deck> deck); //option screen to give user choices on what they want to do
+	bool operator==(person::User account1, person::User account2); //compares two account names and passwords
+	void saveProgress(person::User player);
 
-	person::User makeUser();
-	void newDeck(std::vector<card::Card>& deck);
-	card::Card drawCard(card::Deck& deck);
-	short playerTurn(person::User player, card::Deck& deck);
-	std::string cardName(card::Card& card);
-	short houseTurn(short playerScore, card::Deck& currentDeck);
-	void menu(person::User player, card::Deck& currentDeck);
-	bool operator==(person::User account1, person::User account2);
 
 }
 #endif
